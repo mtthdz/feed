49c594b95705b32c63cb039f550e68e51568b5f1
mtthdz <matt@mtthdz.com>
2023-03-18 13:20:51 -0400

    fontawesome support

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from '@/components/GlobalStyle'
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"; 
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
