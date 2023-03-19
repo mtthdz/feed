24f0b8c3dc64f58f7b04dfad2d389a51c1355094
mtthdz <matt@mtthdz.com>
2023-03-19 14:32:36 -0400

    implementd layout component

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,16 +3,15 @@ import { GlobalStyles } from '@/components/styles/GlobalStyles'
 import { WrapperStyles } from '@/components/styles/WrapperStyles';
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"; 
+import Layout from '@/components/sections/Layout/Layout';
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <GlobalStyles />
-      <WrapperStyles>
-        <Component {...pageProps} />
-      </WrapperStyles>
-    </>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
