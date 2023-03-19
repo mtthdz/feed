10a12261a5d30dea28090059f03ded523e9497a2
mtthdz <matt@mtthdz.com>
2023-03-19 14:32:27 -0400

    created primary nav component

---
+++ client/components/sections/PrimaryNav/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export default function PrimaryNav() {
+  return (
+    <NavStyles>
+      <p>feed</p>
+      <p>post</p>
+      <p>search</p>
+    </NavStyles>
+  )
+}
+
+const NavStyles = styled.section`
+`;
