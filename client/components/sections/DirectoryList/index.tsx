04207afe2eba557577ffeee381627c0420a7f958
mtthdz <matt@mtthdz.com>
2023-03-21 21:02:28 -0400

    created main directory page componnet

---
+++ client/components/sections/DirectoryList/index.tsx
@@ -0,0 +1,24 @@
+import { sampleEntries } from "@/api/sampleData";
+import PostCard from "@/components/elements/PostCard";
+
+export interface PostMetadata {
+  number?: number;
+  title: string;
+  karma?: number;
+  saved: boolean;
+  hidden: boolean;
+  user?: string;
+  date?: string;
+}
+
+export default function DirectoryList() {
+  const postData = sampleEntries;
+
+  return (
+    <>
+      {postData.map((singlePost, index) => {
+        return <PostCard metadata={singlePost} key={index} />
+      })}
+    </>
+  )
+}
--- client/pages/directory.tsx
+++ client/pages/directory.tsx
@@ -1,7 +1,15 @@
+import PostCard from "@/components/elements/PostCard";
+import DirectoryList from "@/components/sections/DirectoryList";
+import styled from "styled-components"
+
 export default function DirectoryPage() {
   return (
-    <>
-      <p>this is the main page of feed.</p>
-    </>
+    <DirectoryStyles>
+      <DirectoryList />
+    </DirectoryStyles>
   )
 }
+
+const DirectoryStyles = styled.div`
+  margin-top: 50px;
+`;
