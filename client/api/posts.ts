6ced8904ec72197c15c427892d8651bd9230ba7d
mtthdz <matt@mtthdz.com>
2023-04-02 14:12:05 -0400

    added interfaces to api endpoints

--- client/api/posts.ts
+++ client/api/posts.ts
@@ -1,177 +1,150 @@
-/**
- * post
- * id: number
- * title: string
- * url: string
- * karma: number
- * comments: arr[]
- * user: string
- * date: string
- */
+export interface PostMeta {
+  id: string,
+  title: string;
+  url: string;
+  date: string;
+  user: string;
+  karma: number;
 
-export const sampleEntries = [
+  // TODO: remove number and change to api call/pagination
+  // TODO: cross reference with userdata
+  // hidden
+  // saved
+}
+
+export const samplePosts = [
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 1,
     title: 'Launch HN: Bloop (YC S21) - Code Search with GPT-4',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'mtthdz',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 2,
     title: 'A world to win: WebAssembly for the rest of us',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'luxe',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 3,
     title: 'CoLT5: Faster Long-Range Transformers With Conditional Computation',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false, 
     user: 'freaksandgeeks',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 4,
     title: 'Zero-1-to-3: Zero-shot One Image to 3D Object',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'disadvantage',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 5,
     title: 'Laying Out a Print Book with CSS',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'ouiskw',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 6,
     title: 'Louis Rossmann could sue John Deere for GPL violation',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'jiripospisil',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 7,
     title: 'An Introduction to Computer Networks (2020)',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'brudgers',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 8,
     title: 'Web fingerprinting is worse than I thought',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'Bright_Machine',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 9,
     title: 'The Yamaha NS10 Story (2008)',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'teleforce',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 10,
     title: 'Doors I touched today (1999)',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'theden',
     date: '',
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 11,
     title: 'Awesome-totally-open-ChatGPT: A list of open alternatives to ChatGPT',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'pallas_athena',
     date: ''
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 12,
     title: 'OpenAI to discontinue support for the Codex API',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'luismedel',
     date: ''
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 13,
     title: 'Paving the Road to Vulkan on Asahi Linux',
     url: 'https://google.ca',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'dannypostma',
     date: ''
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 14,
     title: 'MIT\'s Barry Duncan demonstrates the power of writing in reverse',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'wellokthen',
     date: ''
   },
   {
-    id: 92840238421,
+    id: '92840238421',
     number: 15,
     title: 'In-Flight Entertainment Challenge',
     url: '',
     karma: 2,
-    saved: false,
-    hidden: false,
     user: 'telotortium',
     date: '',
   },
