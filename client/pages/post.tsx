abf99cd002e0e24a9da7abaae6e9fe399fdc0ad6
mtthdz <matt@mtthdz.com>
2023-04-02 14:12:29 -0400

    created post page (incomplete

--- client/pages/post.tsx
+++ client/pages/post.tsx
@@ -1,7 +1,23 @@
+import CardNav from "@/components/containers/CardNav";
+import PostCard from "@/components/containers/PostCard";
+
 export default function PostPage() {
+  let sampleData = {
+    id: 92840238421,
+    number: 1,
+    title: 'Launch HN: Bloop (YC S21) - Code Search with GPT-4',
+    url: '',
+    karma: 2,
+    saved: false,
+    hidden: false,
+    user: 'mtthdz',
+    date: '',
+    content: '',
+  };
+
   return(
-    <>
-      <p>this is a post.</p>
-    </>
+    <div>
+      <PostCard props={{ Metadata: sampleData }} />
+    </div>
   )
 }
