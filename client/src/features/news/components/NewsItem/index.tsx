0fbbac79d3ea1b3895ab8e92d6db39159ce33f8d
mtthdz <matt@mtthdz.com>
2024-10-17 19:41:48 -0400

    (new) news item feature

--- client/src/features/news/components/NewsItem/index.tsx
+++ client/src/features/news/components/NewsItem/index.tsx
@@ -1,7 +1,27 @@
-export default function NewsItem() {
+import { IStory } from '@/types/API';
+import styles from './NewsItem.module.css';
+import ItemNav from '../ItemNav';
+
+export default function NewsItem(props: { story: IStory, order: number }) {
+  const story = props.story;
+  const URLDomain = story.url_ref ?
+    story.url_ref
+      .replace(/(https?:\/\/(www\.)?)/, '') : null;
+
   return (
-    <div>
-      <p>news</p>
+    <div className={styles.container}>
+      <p className={styles.itemOrder}>{props.order}</p>
+      <div className={styles.navContainer}>
+        <p className={styles.itemTitle}>
+          {story.title}
+          {
+            URLDomain
+            ? <span className={styles.itemSubtitle}>({URLDomain})</span>
+            : null
+          }
+        </p>
+        <ItemNav story={story} />
+      </div>
     </div>
   )
 }
--- client/src/features/news/components/NewsList/index.tsx
+++ client/src/features/news/components/NewsList/index.tsx
@@ -1,10 +1,11 @@
 import { IStory } from '@/types/API';
 import styles from './NewsList.module.css'
+import NewsItem from '../NewsItem';
 
 export default function NewsList(props: { news: IStory[] }) {
-  console.log(props);
-
   return (
-    <div className={styles.container}><p>news</p></div>
+    <div className={styles.container}>
+      {props.news.map((story, index) => <NewsItem story={story} order={index + 1} key={story.id} />)}
+    </div>
   )
 }
