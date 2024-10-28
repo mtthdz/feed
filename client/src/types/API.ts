5acac40dacccd9cbabf8eb3f80ed975b93811509
mtthdz <matt@mtthdz.com>
2024-10-28 17:09:13 -0400

    (update) restructured post interface and mock data

--- client/src/types/API.ts
+++ client/src/types/API.ts
@@ -5,42 +5,18 @@ export interface IUser {
   upvote_count: number;
 }
 
-export interface IStory {
-  id: string;
-  title: string;
-  user_id: string;
-  user_name: string;
-  created_at: string;
-  url_ref: string | null;
-  content: string | null;
-  comment_count: number;
-  upvote_count: number;
-  descendants: number; // total comment count
-}
-
-export interface IComment {
-  id: string;
-  post_id: string;
-  user_id: string;
-  user_name: string;
-  created_at: string;
-  content: string;
-  parent_ref: string;
-  child_ref: string[];
-  upvote_count: number;
-}
-
 // folding story and comment types into one to 
 // reflect potential API structure
 export interface IPost {
   id: string;
+  created_at: string;
   user: string;
   user_id: string;
-  title: string | null;
-  content: string | null;
-  url: string | null;
-  parent: string | null;
-  children: string[];
+  title: null | string;
+  content: null | string;
+  url: null | string;
+  parent: null | string;
+  children: IPost[];
   descendants: number;
   score: number;
 }
